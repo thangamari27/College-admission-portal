bced98c870825dd83bdbd507c98d35daf294bcbd
thangamari27 <144316551+thangamari27@users.noreply.github.com>
2025-02-13 10:09:53 -0800

    Delete backend directory

--- backend/routes/studentRoutes.js
+++
@@ -1,9 +0,0 @@
-// routes/studentRoutes.js
-const express = require('express');
-const { getStudentDashboard } = require('../controllers/studentController');
-const authenticateToken = require('../middleware/authMiddleware');
-const router = express.Router();
-
-router.get('/dashboard', authenticateToken, getStudentDashboard);
-
-module.exports = router;
