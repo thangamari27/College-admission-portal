98d17d467071e7c67de848f4cebdcd4912d9b741
thangamari27 <144316551+thangamari27@users.noreply.github.com>
2025-04-18 00:54:50 -0700

    Delete backend directory

--- backend/controllers/homeController.js
+++
@@ -1,7 +0,0 @@
-exports.renderHome = (req, res) => {
-    res.render('college-home', { user: req.user });
-  };
-  
-exports.renderHome = (req, res) => {
-  res.render('student-home', { user: req.user });
-};
